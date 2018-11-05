ced7fb197589644061dfe17c1cfec18b86f314b0
github username <github email address>
2018-11-04 19:04:03 -0600

    starting addition of redux to index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'redux'
+import { createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+
+
 
 ReactDOM.render(
-  <App />,
+  <Provider >
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
 
