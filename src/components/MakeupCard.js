82ff46ec16c619a3d18c892529158b3674877ca0
github username <github email address>
2018-11-08 19:11:55 -0600

    adding makeup/:id view with css and state data

--- src/components/MakeupCard.js
+++ src/components/MakeupCard.js
@@ -10,7 +10,7 @@ class MakeupCard extends React.Component  {
         <div key={makeup.id} className="MakeupCard">
           <img className="MakeupImage" src={makeup.img_url} alt={makeup.name}/>
           <div className="namePriceDescrip">
-            <Link to={{pathname:`/Makeup/${makeup.id}`, state: {message: "hello, im a passed message!"}  }} >{makeup.name}</Link>
+            <Link to={`/Makeup/${makeup.id}`}>{makeup.name}</Link>
             <p> ${makeup.price}</p>
             <p> {makeup.description} </p>
 
--- src/components/MakeupItem.js
+++ src/components/MakeupItem.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { getMakeup } from '../actions/makeup'
+import './makeupItem.css'
 // import { Link } from 'react-router-dom';
 
 class MakeupItem extends React.Component {
 
   componentDidMount() {
   this.props.getMakeup()
-
   }
 
   render() {
-    console.log(this.props.match.params.id)
-  return(
-    <div className="MakeupItemContainer">
-  
-       )}
+  const filteredItem = this.props.makeup.filter(makeup => makeup.id === parseInt(this.props.match.params.id))
+  console.log(filteredItem)
+
+  const filteredArray = filteredItem.map(makeup =>
+    <div>
+    <img className="MakeupItemImage" src={makeup.img_url} alt={makeup.name}/>
+    <h1 className="makeupItemName"> {makeup.name} </h1>
+    <p className="makeupItemPrice">  ${makeup.price} </p>
+    <p className="makeupItemDescription"> {makeup.description} </p>
     </div>
+
+  )
+  return (
+    <div className="makeupItemContainer">
+    {filteredArray}
+    </div>
+
     )
   }
 }
+
+
 
 const mapStateToProps = (state) => {
   return({
